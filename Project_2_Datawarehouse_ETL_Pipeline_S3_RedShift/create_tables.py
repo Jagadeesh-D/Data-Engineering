7005ccb18ddc46ccc219091e51cf18b18ffd0848
Doddapaneni <jdoddapaneni@outlook.com>
2020-01-29 15:48:24 -0500

    Project-2-Adding docstring for each function

--- Project_2_Datawarehouse_ETL_Pipeline_S3_RedShift/create_tables.py
+++ Project_2_Datawarehouse_ETL_Pipeline_S3_RedShift/create_tables.py
@@ -4,12 +4,38 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    drops all tables
+
+    reads drop queries defined for each table in the list object and 
+    executes drop process one table at a time
+
+    Arguments:
+    1 - Cur: cursor object to target database connection
+    2 - Conn: target database connection object.
+
+    Returns:
+    None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    creates all tables
+
+    reads create queries defined for each table in the list object and 
+    executes create process one table at a time
+
+    Arguments:
+    1 - Cur: cursor object to target database connection
+    2 - Conn: target database connection object.
+
+    Returns:
+    None
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- Project_2_Datawarehouse_ETL_Pipeline_S3_RedShift/etl.py
+++ Project_2_Datawarehouse_ETL_Pipeline_S3_RedShift/etl.py
@@ -4,12 +4,38 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    populates staging tables
+
+    reads copy command defined for each staging table in the list object and 
+    executes copy process one by one
+
+    Arguments:
+    1 - Cur: cursor object to target database connection
+    2 - Conn: target database connection object.
+
+    Returns:
+    None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    populates dimension and fact tables
+
+    reads insert queries defined for each table in the list object and 
+    executes inserts process one table at a time
+
+    Arguments:
+    1 - Cur: cursor object to target database connection
+    2 - Conn: target database connection object.
+
+    Returns:
+    None
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
